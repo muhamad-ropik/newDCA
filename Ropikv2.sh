9f2be029b6d58314c63486ff8504b998f96ec46f
MR.R021 <53411066+sayaganteng93123@users.noreply.github.com>
2019-08-08 18:30:24 +0700

    Add files via upload

---
+++ Ropikv2.sh
@@ -0,0 +1,331 @@
+#####################################
+# __  __ ____    ____   ___ ____  - #
+#|  \/  |  _ \  |  _ \ / _ \___ \/ |#
+#| |\/| | |_) | | |_) | | | |__) | |#
+#| |  | |  _ < _|  _ <| |_| / __/| |#
+#|_|  |_|_| \_(_)_| \_\\___/_____|_|#
+#####################################
+#MAU NGAPAIN AJG
+#MAU DIRECODE? KYA YG KEMAREN
+#GW BIKIN TOOLS INI CAPEK HARGAI KARYA ORANG
+#KLO MAU SHARE TOOLS SEDERHANA INI KE TMN²
+#ANDA KAN NGERTI PYTHON PYTHON2 PYTHON3
+#JADI KNPA GK BIKIN SENDIRI AJA?
+
+clear
+bi='\033[34;1m' #BIRU
+i='\033[32;1m' #IJO
+pur='\033[35;1m' #PURPLE
+cy='\033[36;1m' #CYAN
+me='\033[31;1m' #MERAH
+pu='\033[37;1m' #PUTIH
+ku='\033[33;1m' #KUNING
+echo
+echo
+echo
+clear
+echo
+echo $red" Tool akan di install dalam waktu 5 detik saja"
+echo $red" harap sabar menunggu WIBUKU:V :)"   
+sleep 1
+echo '''\a
+\033[34;1m               _
+\033[34;1m              / |
+\033[34;1m              | |
+\033[34;1m              | |
+\033[34;1m             _|_|_ '''
+sleep 1
+apt update && apt upgrade
+apt install nano figlet curl
+clear
+echo
+echo
+echo $red" Tool akan di install dalam waktu 5 detik saja"
+echo $red" harap sabar menunggu WIBUKU:V :)"
+sleep 1
+echo '''\a
+\033[32;1m              ____
+\033[32;1m             |___ \
+\033[32;1m               __) |
+\033[32;1m              / __/
+\033[32;1m             |_____| '''
+sleep 1
+pip2 install mechanize
+pip2 install lolcat
+pip2 install requests
+clear
+echo
+echo
+echo $red" Tool akan di install dalam waktu 5 detik saja"
+echo $red" harap sabar menunggu WIBUKU:V :)"
+sleep 1
+echo '''\a
+\033[35;1m             _____
+\033[35;1m            |___ /
+\033[35;1m              |_ \
+\033[35;1m             ___) |
+\033[35;1m            |____/ '''
+sleep 1
+clear
+echo
+echo
+echo $red" Tool akan di install dalam waktu 5 detik saja"
+echo $red" harap sabar menunggu WIBUKU:V :)"
+sleep 1
+echo '''\a
+\033[33;1m             _  _
+\033[33;1m            | || |
+\033[33;1m            | || |_
+\033[33;1m            |__   _|
+\033[33;1m               |_| '''
+sleep 1
+clear
+echo
+echo
+echo $red" Tool akan di install dalam waktu 5 detik saja"
+echo $red" harap sabar menunggu WIBUKU:V :)"
+sleep 1
+echo '''\a
+\033[36;1m              ____
+\033[36;1m             | ___|
+\033[36;1m             |___ \
+\033[36;1m              ___) |
+\033[36;1m             |____/ '''
+sleep 1
+clear
+echo
+clear
+echo "Loading .."
+load
+clear
+echo Selamat datang nyet, Siapa nick ente? #tulisan keluar
+read nick #membaca yang ditulis
+clear
+echo Selamat datang $nick ":)" JOMBLO! :v
+echo $cy"==================="$pur" ==================="
+echo $me"█"$bi"  Author  : MR.R021          "$me
+echo $ku"█"$bi"  WhatsApp: 0882xxxxx"$ku 
+echo $bi"█"$bi"  Team: DARK CYBER ARMY"$bi
+echo $i"█"$bi"  Youtube : MR.R021 Tutorial"$i
+echo $me"==================="$ku" ==================="
+echo
+echo $i">>>>>>>>> Ini Namanya Menu:v"
+echo $cy"⟨"$pu"1"$cy"⟩"$bi" -» Report FB"
+echo $cy"⟨"$pu"2"$cy"⟩"$bi" -» Hack Fb (Dark-Fb)"
+echo $cy"⟨"$pu"3"$cy"⟩"$bi" -» Brute Force Facebook Cracker"
+echo $cy"⟨"$pu"4"$cy"⟩"$bi" -» Mbf"
+echo $cy"⟨"$pu"5"$cy"⟩"$bi" -» Cloning Yahoo"
+echo $cy"⟨"$pu"6"$cy"⟩"$bi" -» OsiF"
+echo $cy"⟨"$pu"7"$cy"⟩"$bi" -» Bot Reaction Facebook"
+echo $cy"⟨"$pu"8"$cy"⟩"$bi" -» Profileguard Facebook"
+echo $cy"⟨"$pu"9"$cy"⟩"$bi" -» Spam Sms Unlimited!"
+echo $cy"⟨"$pu"10"$cy"⟩"$bi"-» Spam Call [FREE]"
+echo $cy"⟨"$pu"11"$cy"⟩"$bi"-» Sadap Whatsapp"
+echo $cy"⟨"$pu"12"$cy"⟩"$bi"-» Youtube-dl"
+echo $cy"⟨"$pu"13"$cy"⟩"$bi"-» Tools Trojan Perusak"
+echo $cy"⟨"$pu"14"$cy"⟩"$bi"-» DDOS TERBARU"
+echo $cy"⟨"$pu"15"$cy"⟩"$bi"-» SPAM GMAIL"
+echo $cy"⟨"$pu"16"$cy"⟩"$bi"-» TOOLS BUCIN (Semoga Selamat)"
+echo $cy"⟨"$pu"17"$cy"⟩"$bi"-» Keluar"
+echo
+echo $pu"╭─["$i"Pilih sesuai ke inginan Anda"$pu"]"
+read -p"╰─> : " pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "Lest G O ."|lolcat
+sleep 1
+git clone https://github.com/IlayTamvan/Report.git
+cd Report
+unzip Report.zip
+python2 Report.py
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "Lest G O ."|lolcat
+sleep 1
+git clone https://github.com/SantriCyber/santri-IDN
+cd santri-IDN
+python2 santri-IDN.py
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "Lest G O ."|lolcat
+sleep 1
+git clone https://github.com/Edi-nasa47/7.git
+cd 7
+python2 crack.py
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "Lest G O ."|lolcat
+sleep 1
+git clone https://github.com/Edi-nasa47/1.git
+cd 1
+python2 MBF.py
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "Lest G O ."|lolcat
+sleep 1
+git clone https://github.com/Edi-nasa47/2.git
+cd 2
+pip2 install requests mechanize
+pip2 install requirements
+python2 boss.py
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "Lest G O ."|lolcat
+sleep 1
+git clone https://github.com/Edi-nasa47/8.git
+cd 8
+pip2 install -r requirements.txt
+python2 osif.py
+fi
+
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant "Lest G O ."|lolcat
+sleep 1
+git clone https://github.com/Edi-nasa47/5.git
+cd 5
+python2 robot.py
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "Lest G O ."|lolcat
+sleep 1
+git clone https://github.com/Edi-nasa47/13.git
+cd 13
+php profile.php
+fi
+
+if [ $pil = 9 ]
+then
+clear
+figlet -f slant "Lest G O ."|lolcat
+sleep 1
+git clone https://github.com/Muhamad-Ropik/SpamSmsUnlimited
+cd SpamSmsUnlimited
+php Spam-Pik.php
+fi
+
+if [ $pil = 10 ]
+then
+clear
+figlet -f slant "Lest G O ."|lolcat
+sleep 1
+git clone https://github.com/Muhamad-Ropik/SpamCallFree
+cd SpamCallFree
+php CallPik.php
+fi
+
+if [ $pil = 11 ]
+then
+clear
+figlet -f slant "Lest G O ."|lolcat
+sleep 1
+git clone https://github.com/Muhamad-Ropik/WhatsRop
+cd WhatsRop
+sh WhatsRop.sh
+fi
+
+if [ $pil = 12 ]
+then
+clear
+figlet -f slant "Lest G O ."|lolcat
+sleep 1
+git clone https://github.com/Edi-nasa47/6.git
+cd 6
+sh youtube-dl.sh
+fi
+
+if [ $pil = 13 ]
+then
+clear
+figlet -f slant "Lest G O ."|lolcat
+sleep 1
+git clone https://github.com/justahackers/perusak
+cd perusak
+python2 perusak.py
+fi
+
+if [ $pil = 14 ]
+then
+clear
+figlet -f slant "Lest G O ."|lolcat
+sleep 1
+git clone https://github.com/Muhamad-Ropik/DDOSnew
+cd DDOSnew
+python2 DdosPik.py
+fi
+
+if [ $pil = 15 ]
+then
+clear
+figlet -f slant "Lest G O ."|lolcat
+sleep 1
+git clone https://github.com/Muhamad-Ropik/DDOSnew
+cd Spam-Gmail
+php Spam-Pik.php
+fi
+
+if [ $pil = 16 ]
+then
+clear
+figlet -f slant "Lest G O ."|lolcat
+sleep 1
+git clone https://github.com/Muhamad-Ropik/Bucin-Tools
+cd Bucin-Tools
+python2 Tools-Bucin.py
+fi
+
+if [ $pil = 17 ]
+then
+clear
+figlet -f slant "E X I T"|Ulolcat
+sleep 2
+echo $i"Good Bay Nyett..."
+echo $i"Terima Kasih Budjank sudah mengunakan Tools ini"
+sleep 2
+echo $i"Silahkan Budjank di pakai semoga bermanfaat"
+sleep 2
+echo $i"Buat Wibu²"
+sleep 2
+echo $i"Bila Budjank belum mengerti Akang Bisa Nanya"
+echo $i"Melalui Via:"
+sleep 2
+echo $ku"Whatsapp :"$pur" 088213051360"
+echo $ku"YouTube  :"$pur" MR.R021 Tutorial"
+sleep 2
+echo $i"dan jangan lupa "$cy" LIKE SHARE AND SUBSCRIBE"
+sleep 2
+echo $i"Video Channel "$cy" MR.R021 Tutorial"
+sleep 2
+echo $i"Agar tambah semangat Upload Video²"
+sleep 2
+echo $i"terbaru menarik dari saya"
+echo $i"silahkan Aktifkan juga ya Budjank Tombol Loncengnya"
+sleep 2
+echo $i"Agar dapat pemberitahuan Video²"
+echo $i"selanjutnya dari saya OKE"
+sleep 2
+echo
+echo $cy"Thanks FOR WATCHING....!!!"
+exit
+fi
